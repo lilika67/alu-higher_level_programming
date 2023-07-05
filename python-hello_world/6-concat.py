a3523f86773414c3b86f9cb25a3081ea42d55772
lilika67 <l.kayitesi@alustudent.com>
2023-07-05 16:28:06 -0700

    6-concat.py

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str11 = "Battery"
-str22 ="Street"
-res = f"{str11} + {str22}"
+string1 = "Holberton"
+string2 ="School"
+res = f"{string1}  {string2}"
 print(f"Welcome to {res}!")

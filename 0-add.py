5f4857f00d658e8452f37f1a17763355d043f05b
lilika67 <l.kayitesi@alustudent.com>
2023-07-10 08:08:00 -0700

    0

--- 0-add.py
+++ 0-add.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
+
 from add_0 import add
 
-
-def main():
+if __name__ == "__main__":
     a = 1
     b = 2
-    c = add(a, b)
-
-    print("{} + {} = {}".format(a, b, c))
-
-
-if __name__ == "__main__":
-    main()
+    print("{a} + {b} = {result}".format(a=a, b=b, result=add(a, b)))

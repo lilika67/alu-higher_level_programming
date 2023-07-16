b56baf5e20e54623a309645b550f6740846e20bc
lilika67 <l.kayitesi@alustudent.com>
2023-07-16 12:05:01 -0700

    4

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-
-
-def new_in_list(my_list, idx, element):
-    """Replace an element in a copied list at a specific position."""
-    if idx < 0 or idx > (len(my_list) - 1):
-        return (my_list)
-
-    copy = [x for x in my_list]
-    copy[idx] = element
-    return (copy)
+def only_diff_elements(set_1, set_2):
+    return (set_1 ^ set_2)

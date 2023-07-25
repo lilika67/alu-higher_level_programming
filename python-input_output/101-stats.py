ae456dcd68a3b1b6265de0c01c85ed758d6e6a95
lilika67 <l.kayitesi@alustudent.com>
2023-07-25 06:35:43 -0700

    14

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,42 +1,52 @@
 #!/usr/bin/python3
-'''
-Reading data from stdout
-'''
-import sys
-import re
+# script that reads stdin line and computes metrics
+"""define a function 'print_stats'."""
 
 
-def print_data(code_count, total_size):
-    '''
-    Prints data summary
-    '''
-    print("File size: {}".format(total_size))
-    for key, value in sorted(code_count.items()):
-        if value > 0:
-            print("{key}: {value}".format(key=key, value=value))
+def print_stats(size, status_codes):
+    """
+        print accumulated metrics
+    """
+
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
 
 
-i = 0
-pattern = (r'(\d+\.\d+\.\d+\.\d+) - \[(.*?)\] "GET /projects/(\d+) '
-           r'HTTP/\d\.\d" (\d+) (\d+)')
-status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0,
-                      403: 0, 404: 0, 405: 0, 500: 0}
-total_file_size = 0
-try:
-    for line in sys.stdin:
-        i += 1
-        match = re.match(pattern, line)
-        if match:
-            ip_address = match.group(1)
-            date = match.group(2)
-            project_id = match.group(3)
-            status_code = match.group(4)
-            file_size = match.group(5)
+if __name__ == "__main__":
+    import sys
 
-        status_code_counts[int(status_code)] += 1
-        total_file_size += int(file_size)
-        if i == 10:
-            i = 0
-            print_data(status_code_counts, total_file_size)
-except KeyboardInterrupt:
-    print_data(status_code_counts, total_file_size)
+    size = 0
+    status_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
+
+    try:
+        for line in sys.stdin:
+            if count == 10:
+                print_stats(size, status_codes)
+                count = 1
+            else:
+                count += 1
+
+            line = line.split()
+
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stats(size, status_codes)
+
+    except KeyboardInterrupt:
+        print_stats(size, status_codes)
+        raise

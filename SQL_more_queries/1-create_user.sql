1eec9a740996dd924af2b3fa1c164b37bb75ea90
lilika67 <l.kayitesi@alustudent.com>
2023-08-02 01:22:18 -0700

    1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
--- Creates user user_0d_1 with password user_0d_1_pwd
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- Create A USER
+CREATE USER IF NOT EXISTS
+user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON
+*.* TO user_0d_1@localhost;
